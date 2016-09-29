02ce91fbd52bd5280c7488080172c6ca9352eff2
Leone Parise <leone.parise@gmail.com>
2016-09-29 19:29:16 -0300

    Fix project to support carthage

---
+++ LLSimpleCamera/LLSimpleCamera-Umbrella.h
@@ -0,0 +1,19 @@
+//
+//  LLSimpleCamera-umbrella.h
+//  LLSimpleCameraExample
+//
+//  Created by Leone Parise Vieira da Silva on 29/09/16.
+//  Copyright © 2016 Ömer Faruk Gül. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LLSimpleCamera.
+FOUNDATION_EXPORT double LLSimpleCameraVersionNumber;
+
+//! Project version string for LLSimpleCamera.
+FOUNDATION_EXPORT const unsigned char LLSimpleCameraVersionString[];
+
+#import <LLSimpleCamera/LLSimpleCamera.h>
+#import <LLSimpleCamera/LLSimpleCamera+Helper.h>
+#import <LLSimpleCamera/UIImage+FixOrientation.h>
